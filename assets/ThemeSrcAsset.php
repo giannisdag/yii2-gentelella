bd26db989615ed69acdbd985985295a6b9b18c8d
giannisdag <giannisdag@yahoo.gr>
2020-01-24 12:53:23 +0200

    set build path, added min js

--- assets/ThemeSrcAsset.php
+++ assets/ThemeSrcAsset.php
@@ -11,10 +11,11 @@ use yii\web\AssetBundle;
 
 class ThemeSrcAsset extends AssetBundle
 {
-    public $sourcePath = '@bower/gentelella/src/js';
+    public $sourcePath = '@bower/gentelella/build/js';
 
     public $js = [
         'helpers/smartresize.js',
         'custom.js',
+        'custom.min.js',
     ];
 }
